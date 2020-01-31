f10e57f97be470bd4c149579e429aaff9bcf25d1
Envel Le Hir <envel@lehir.net>
2020-01-31 19:56:21 +0100

    Twitter bot Wikidata events: added discoveries and score tuning

--- scripts/wikidata-events.php
+++ scripts/wikidata-events.php
@@ -2,39 +2,56 @@
 
 require '../inc/load.inc.php';
 
-define('MIN_SITELINKS', 17);
-$date2 = new DateTime(date('Y-m-d'));
-$dates = array();
+define('MIN_YEAR', 1000);
 
-$results1 = @wdqs::query('SELECT ?item ?date ?sitelinks {
+$date2 = new DateTime(date('Y-m-d'));
+$events = array();
+
+# events
+echo 'Events...'."\n";
+$res = @wdqs::query('SELECT ?item ?date ?sitelinks {
   ?item p:P585 [ rdf:type wikibase:BestRank ; psv:P585 [ wikibase:timeValue ?date ; wikibase:timePrecision ?precision ] ] ; wikibase:sitelinks ?sitelinks .
-  FILTER(?precision >= 11 && YEAR(?date) >= 1400 && ?sitelinks >= '.MIN_SITELINKS.') .
+  FILTER(?precision >= 11 && YEAR(?date) >= '.MIN_YEAR.' && ?sitelinks >= 20 && ?item != wd:Q668984) .
   MINUS { ?item wdt:P31 wd:Q47150325 } . # simple date
   MINUS { ?item wdt:P31 wd:Q16913666 } . # Academy Awards ceremony
+  MINUS { ?item wdt:P31 wd:Q276 } . # Eurovision
   MINUS { ?item wdt:P31/wdt:P279 wd:Q65742449 } . # Formula One race
   MINUS { ?item wdt:P31/wdt:P279 wd:Q858439 } . # presidential election
-}
-ORDER BY ?date ?sitelinks
-#', 86400)->results->bindings;
-if (empty($results1)) {
+}', 86400)->results->bindings;
+if (empty($res)) {
     die('Wikidata query failed.'."\n");
 }
-
-foreach ($results1 as $event1) {
-    $dates[substr($event1->date->value, 0, 10)] = $event1;
+foreach ($res as $event) {
+    addEvent($events, $event, 'event');
 }
 
+# discoveries
+echo 'Discoveries...'."\n";
+$res = @wdqs::query('SELECT ?item ?date ?sitelinks {
+  ?item p:P575 [ rdf:type wikibase:BestRank ; psv:P575 [ wikibase:timeValue ?date ; wikibase:timePrecision ?precision ] ] ; wikibase:sitelinks ?sitelinks .
+  FILTER(?precision >= 11 && YEAR(?date) >= '.MIN_YEAR.' && ?sitelinks >= 50) .
+  #MINUS { ?item wdt:P31/wdt:P279* wd:Q17444909 } . # astronomical object
+  #MINUS { ?item wdt:P31/wdt:P279*/wdt:P31 wd:Q17444909 } . # astronomical object
+}', 86400)->results->bindings;
+if (empty($res)) {
+    die('Wikidata query failed.'."\n");
+}
+foreach ($res as $event) {
+    addEvent($events, $event, 'discovery');
+}
+
+# TODO
+# https://www.wikidata.org/wiki/Q631114#P1619
+
 echo '"Event 0","URL 0","Date 0","Sitelinks 0","Event 1","URL 1","Date 1","Sitelinks 1","Score"'."\n";
-foreach ($dates as $event1) {
-    $date1 = new DateTime(substr($event1->date->value, 0, 10));
+foreach ($events as $event1) {
+    $date1 = new DateTime($event1->date);
     $interval = $date1->diff($date2);
     $date0 = clone $date1;
     $date0 = $date0->sub($interval);
-    if (isset($dates[$date0->format('Y-m-d')])) {
-        $event0 = $dates[$date0->format('Y-m-d')];
-        $qid0 = substr($event0->item->value, 31);
-        $qid1 = substr($event1->item->value, 31);
-        echo '"'.getLabel($qid0).'","https://www.wikidata.org/wiki/'.$qid0.'","'.substr($event0->date->value, 0, 10).'",'.$event0->sitelinks->value.',"'.getLabel($qid1).'","https://www.wikidata.org/wiki/'.$qid1.'","'.substr($event1->date->value, 0, 10).'",'.$event1->sitelinks->value.','.getScore($event0, $event1)."\n";
+    if (isset($events[$date0->format('Y-m-d')])) {
+        $event0 = $events[$date0->format('Y-m-d')];
+        echo '"'.getLabel($event0).'","https://www.wikidata.org/wiki/'.$event0->qid.'","'.$event0->date.'",'.$event0->sitelinks.',"'.getLabel($event1).'","https://www.wikidata.org/wiki/'.$event1->qid.'","'.$event1->date.'",'.$event1->sitelinks.','.getScore($event0, $event1)."\n";
         if (!isset($selected_event0) || (getScore($selected_event0, $selected_event1) < getScore($event0, $event1))) {
             $selected_event0 = $event0;
             $selected_event1 = $event1;
@@ -42,16 +59,33 @@ foreach ($dates as $event1) {
     }
 }
 
-function getScore($event0, $event1) {
-    return ($event0->sitelinks->value + $event1->sitelinks->value) * sqrt(1 + (substr($event1->date->value, 0, 4) - substr($event0->date->value, 0, 4)) / 25);
+if (isset($selected_event0)) {
+    $status = 'Random events from Wikidata: the '.getLabel($selected_event1).' is now as close to the present as to the '.getLabel($selected_event0).'.'."\n".$selected_event0->date.' '.getArticle($selected_event0->qid)."\n".$selected_event1->date.' '.getArticle($selected_event1->qid)."\n".$date2->format('Y-m-d').' https://en.wikipedia.org/wiki/Main_Page';
+    echo $status."\n";
+    twitterapi::postTweet($status);
 }
 
-function getLabel($qid) {
-    $r = wdqs::query('SELECT ?label { wd:'.$qid.' rdfs:label ?label . FILTER(LANG(?label) = "en") }', 86400)->results->bindings;
-    if (count($r) == 1) {
-        return $r[0]->label->value;
+function addEvent(&$events, $event, $type) {
+    $date = substr($event->date->value, 0, 10);
+    if (!isset($events[$date]) || ($events[$date]->sitelinks < $event->sitelinks->value)) {
+        $events[$date] = (object) array('qid' => substr($event->item->value, 31), 'date' => $date, 'sitelinks' => $event->sitelinks->value, 'type' => $type);
     }
-    return $qid;
+}
+
+function getScore($event0, $event1) {
+    return ($event0->sitelinks + $event1->sitelinks) * sqrt(1 + (substr($event1->date, 0, 4) - substr($event0->date, 0, 4)) / 25);
+}
+
+function getLabel($event) {
+    $label = $event->qid;
+    $res = wdqs::query('SELECT ?label { wd:'.$event->qid.' rdfs:label ?label . FILTER(LANG(?label) = "en") }', 86400)->results->bindings;
+    if (count($res) == 1) {
+        $label = $res[0]->label->value;
+    }
+    if ($event->type == 'discovery') {
+        $label = 'discovery of '.$label;
+    }
+    return $label;
 }
 
 function getArticle($qid) {
@@ -60,12 +94,6 @@ function getArticle($qid) {
         return $r[0]->article->value;
     }
     return 'https://www.wikidata.org/wiki/'.$qid;
-}
-
-if (isset($selected_event0)) {
-    $status = 'Random events from Wikidata: the '.getLabel(substr($selected_event1->item->value, 31)).' is now as close to the present as to the '.getLabel(substr($selected_event0->item->value, 31)).'.'."\n".substr($selected_event0->date->value, 0, 10).' '.getArticle(substr($selected_event0->item->value, 31))."\n".substr($selected_event1->date->value, 0, 10).' '.getArticle(substr($selected_event1->item->value, 31))."\n".$date2->format('Y-m-d').' https://en.wikipedia.org/wiki/Main_Page';
-    echo $status."\n";
-    twitterapi::postTweet($status);
 }
 
 ?>
