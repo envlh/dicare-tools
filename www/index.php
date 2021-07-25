093f6bc0bc09b9fd39d13b83f0024029d85b287d
Envel Le Hir <envel@lehir.net>
2021-07-25 18:36:11 +0200

    temporarily unlinking tools that probably need some rework

--- www/index.php
+++ www/index.php
@@ -13,15 +13,15 @@ require '../inc/header.inc.php';
 <h2>Tools list</h2>
 <ul>
 <li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.TRANSLATHON_SITE_DIR; ?>">Transl-a-thon</a></strong>: find items needing to be translated in Wikidata</li>
-<li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.PROPERTIES_SITE_DIR; ?>">[Wikidata] Related Properties</a></strong>: explore Wikidata properties and their related siblings</li>
-<li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.PROJECTS_SITE_DIR; ?>">[Wikimedia] Related Projects</a></strong>: find which projects are the closest to each other, given their sitelinks usage</li>
+<!--<li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.PROPERTIES_SITE_DIR; ?>">[Wikidata] Related Properties</a></strong>: explore Wikidata properties and their related siblings</li>-->
+<!--<li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.PROJECTS_SITE_DIR; ?>">[Wikimedia] Related Projects</a></strong>: find which projects are the closest to each other, given their sitelinks usage</li>-->
 <li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.DIFF_SITE_DIR; ?>">Wikidata Diff</a></strong>: compare the properties of two Wikidata items</li>
-<li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.DD_SITE_DIR; ?>">Dedicated Dashboard</a></strong>: another tool to improve the data quality in Wikidata</li>
+<!--<li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.DD_SITE_DIR; ?>">Dedicated Dashboard</a></strong>: another tool to improve the data quality in Wikidata</li>-->
 <li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.BNF_SITE_DIR; ?>">BnF To Wikidata</a></strong>: import sourced data from BnF into Wikidata</li>
 <li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.IDREF_SITE_DIR; ?>">IdRef To Wikidata</a></strong>: import sourced data from IdRef into Wikidata</li>
 <li><span class="language">[en]</span> <strong><a href="<?php echo SITE_DIR.LEXEMES_SITE_DIR; ?>">Lexemes Party</a></strong>: explore lexemes linked to a group of Wikidata concepts</li>
-<li><span class="language">[fr]</span> <strong><a href="<?php echo SITE_DIR.DEPUTES_SITE_DIR; ?>">Députés</a></strong> : statistiques et listes de députés français de la 5<sup>e</sup> République</li>
-<li><span class="language">[fr]</span> <strong><a href="<?php echo SITE_DIR.NOMS_SITE_DIR; ?>">Noms</a></strong>
+<!--<li><span class="language">[fr]</span> <strong><a href="<?php echo SITE_DIR.DEPUTES_SITE_DIR; ?>">Députés</a></strong> : statistiques et listes de députés français de la 5<sup>e</sup> République</li>-->
+<!--<li><span class="language">[fr]</span> <strong><a href="<?php echo SITE_DIR.NOMS_SITE_DIR; ?>">Noms</a></strong>
     <ul>
         <li><a href="<?php echo SITE_DIR.NOMS_SITE_DIR; ?>homonymie.php">Génération d'une page d'homonymie pour la Wikipédia en français</a></li>
         <li><a href="<?php echo SITE_DIR.NOMS_SITE_DIR; ?>nom-de-famille.php">Ajout en masse d'un nom de famille sur Wikidata</a></li>
@@ -30,8 +30,8 @@ require '../inc/header.inc.php';
         <li><a href="<?php echo SITE_DIR.NOMS_SITE_DIR; ?>pays.php">Statistiques par pays sur les noms de famille et les prénoms</a></li>
         <li><a href="<?php echo SITE_DIR.NOMS_SITE_DIR; ?>departements.php">Statistiques par département français sur les noms de famille et les prénoms</a></li>
     </ul>
-</li>
-<li><span class="language">[fr]</span> <strong><a href="<?php echo SITE_DIR.BATEAUX_SITE_DIR; ?>">Listes de bateaux</a></strong></li>
+</li>-->
+<!--<li><span class="language">[fr]</span> <strong><a href="<?php echo SITE_DIR.BATEAUX_SITE_DIR; ?>">Listes de bateaux</a></strong></li>-->
 </ul>
 
 <h2>About</h2>
