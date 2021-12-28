77a789f070d4b4e0f25c3dcf1008b30d66393509
Envel Le Hir <envel@lehir.net>
2021-12-28 18:15:42 +0100

    Lexemes Party: relative time for last data update

--- inc/lib/LexemeParty.class.php
+++ inc/lib/LexemeParty.class.php
@@ -457,13 +457,14 @@ class LexemeParty {
                 echo '</tr>'."\n";
             }
         }
+		$minutesElapsed = floor((time() - strtotime($this->items_query_time)) / 60);
         echo '</table>
     <p>'.count($this->languages).' languages
     &nbsp;&nbsp;&nbsp;&nbsp; <img src="'.SITE_STATIC_DIR.'img/famfamfam/medal_gold_3.png" alt="" class="medal" title="100%" /> '.$this->medals['gold'].'
     &nbsp;&nbsp;&nbsp;&nbsp; <img src="'.SITE_STATIC_DIR.'img/famfamfam/medal_silver_3.png" alt="" class="medal" title="≥ 80%" /> '.$this->medals['silver'].'
     &nbsp;&nbsp;&nbsp;&nbsp; <img src="'.SITE_STATIC_DIR.'img/famfamfam/medal_bronze_3.png" alt="" class="medal" title="≥ 50%" /> '.$this->medals['bronze'].'
     &nbsp;&nbsp;&nbsp;&nbsp; &#8709; '.$this->medals[''].'</p>
-	<p>Last data update: '.$this->items_query_time.'.</p>';
+	<p>Last data update: '.$this->items_query_time.' ('.($minutesElapsed > 0 ? $minutesElapsed.' minute'.($minutesElapsed > 1 ? 's' : '').' ago' : 'now').').</p>';
     }
     
     private function display_cell($items) {
