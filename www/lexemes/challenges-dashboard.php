b48873ffa4dab1359ed2a43f0f695c41e2b9f4d2
Envel Le Hir <envel@lehir.net>
2023-01-06 01:04:30 +0100

    Lexemes Challenges dashboard: 20 lines per table

--- www/lexemes/challenges-dashboard.php
+++ www/lexemes/challenges-dashboard.php
@@ -13,7 +13,7 @@ $concepts_count = db::query('SELECT SUM(`concepts_count`) FROM `lexemes_challeng
 
 echo '<p>These tables count only lexemes for which at least one of the following properties was added or removed during the challenges: '.LexemeChallenge::getPropertiesList().'.</p>';
 
-$res = db::query('SELECT `language_id`, SUM(`completion`) AS `completion`, SUM(`removed`) AS `removed`, SUM(`added`) AS `added` FROM `lexemes_challenge_statistics` GROUP BY `language_id` ORDER BY SUM(`removed` + `added`) DESC, `completion` DESC LIMIT 10');
+$res = db::query('SELECT `language_id`, SUM(`completion`) AS `completion`, SUM(`removed`) AS `removed`, SUM(`added`) AS `added` FROM `lexemes_challenge_statistics` GROUP BY `language_id` ORDER BY SUM(`removed` + `added`) DESC, `completion` DESC LIMIT 20');
 echo '<h2>Most improved languages during challenges</h2>';
 $rankings = array();
 while ($ranking = $res->fetch_object()) {
@@ -22,7 +22,7 @@ while ($ranking = $res->fetch_object()) {
 }
 LexemeParty::displayRankings($rankings, $concepts_count);
 
-$res = db::query('SELECT `language_id`, SUM(`completion`) AS `completion`, SUM(`removed`) AS `removed`, SUM(`added`) AS `added` FROM `lexemes_challenge_statistics` GROUP BY `language_id` ORDER BY `completion` DESC, SUM(`removed` + `added`) DESC LIMIT 10');
+$res = db::query('SELECT `language_id`, SUM(`completion`) AS `completion`, SUM(`removed`) AS `removed`, SUM(`added`) AS `added` FROM `lexemes_challenge_statistics` GROUP BY `language_id` ORDER BY `completion` DESC, SUM(`removed` + `added`) DESC LIMIT 20');
 echo '<h2>Most complete languages at the end of challenges</h2>';
 $rankings = array();
 while ($ranking = $res->fetch_object()) {
